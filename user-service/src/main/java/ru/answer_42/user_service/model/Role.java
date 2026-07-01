b40ee6e03b4a0e99f0f827521b039798bf234349
motr3x <matvey.shcherin@mail.ru>
2026-07-01 16:13:49 +0300

    feat(user-service): add user models

---
+++ user-service/src/main/java/ru/answer_42/user_service/model/Role.java
@@ -0,0 +1,5 @@
+package ru.answer_42.user_service.model;
+
+public enum Role {
+  USER, ADMIN
+}
---
+++ user-service/src/main/java/ru/answer_42/user_service/model/User.java
@@ -0,0 +1,26 @@
+package ru.answer_42.user_service.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+  private UUID id;
+  private String login;
+  private Role role;
+  private List<UUID> files = new ArrayList<>();
+  @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
+  private LocalDateTime createdAt = LocalDateTime.now();
+  @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
+  private LocalDateTime updateDate = LocalDateTime.now();
+}
