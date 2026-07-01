0437fce09c0964474446e9f10f6d29b4a99b0c3c
motr3x <matvey.shcherin@mail.ru>
2026-07-01 16:24:43 +0300

    feat(files-service): add dto utilities

---
+++ file-storage-service/src/main/java/ru/answer_42/file_storage_service/dto/FileRequestDto.java
@@ -0,0 +1,13 @@
+package ru.answer_42.file_storage_service.dto;
+
+import lombok.Data;
+import ru.answer_42.file_storage_service.model.Status;
+import ru.answer_42.file_storage_service.model.Type;
+
+@Data
+public class FileRequestDto {
+  private String title;
+  private Type type;
+  private Long size;
+  private Status status;
+}
