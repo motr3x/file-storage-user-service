bc46f302240a12584fd8b2ffa614ddc426e996b2
motr3x <matvey.shcherin@mail.ru>
2026-07-01 16:41:56 +0300

    feat(user-service): add other files

---
+++ user-service/src/test/java/ru/answer_42/user_service/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.answer_42.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
