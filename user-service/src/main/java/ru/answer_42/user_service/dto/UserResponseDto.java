1ce70856d1095c7e65e9d23594a677c6af57cf8c
motr3x <matvey.shcherin@mail.ru>
2026-07-01 16:16:10 +0300

    feat(user-service): add dto utilities

---
+++ user-service/src/main/java/ru/answer_42/user_service/dto/UserResponseDto.java
@@ -0,0 +1,8 @@
+package ru.answer_42.user_service.dto;
+
+import lombok.Data;
+
+@Data
+public class UserResponseDto {
+  private String login;
+}
---
+++ user-service/src/main/java/ru/answer_42/user_service/mapper/UserMapper.java
@@ -0,0 +1,26 @@
+package ru.answer_42.user_service.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.ReportingPolicy;
+import org.springframework.stereotype.Component;
+import ru.answer_42.user_service.dto.UserRequestDto;
+import ru.answer_42.user_service.dto.UserResponseDto;
+import ru.answer_42.user_service.model.User;
+
+@Mapper(
+    componentModel = "spring",
+    unmappedTargetPolicy = ReportingPolicy.WARN,
+    //Позволяет кидать json с незаполненными полями, при этом старые поля остаются неизменными
+    //годная вещь для обновления данных пользователя, позволяет изменять только имя, при этом
+    //не перезаписывать все остальные данные на нуль
+    nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE
+)
+@Component
+public interface UserMapper {
+  UserResponseDto toUserResponseDto(User user);
+  UserRequestDto toFileDto(User user);
+  User toEntity(UserRequestDto userDto);
+  void updateEntityFromDto(UserRequestDto userDto, @MappingTarget User user);
+}
