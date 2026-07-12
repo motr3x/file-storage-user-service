4bce60ec981670608b65743bac17bd403af4c6b2
motr3x <matvey.shcherin@mail.ru>
2026-07-12 17:09:58 +0300

    feat(user-service): add migration script

--- user-service/src/main/java/ru/answer_42/user_service/service/UserService.java
+++ user-service/src/main/java/ru/answer_42/user_service/service/UserService.java
@@ -11,34 +11,14 @@ import ru.answer_42.user_service.model.FileMetadata;
 public interface UserService {
 
   FileMetadata addFileMetadata(FileMetadata fileMetadata);
+
   List<UserResponseDto> findAll();
-  /**
-   * Создает нового юзера
-   * @param userDto - юзер для создания
-   */
+
   UserResponseDto save(UserRequestDto userDto);
 
-  /**
-   * Обновляет юзера с заданным ID,
-   * в соответствии с переданным юзером
-   * @param userDto - юзер в соответствии с которым нужно обновить данные
-   * @param id - id юзера которой нужно обновить
-   * @return - объект UserResponseDto
-   */
   UserResponseDto update(UUID id, UserRequestDto userDto);
 
-  /**
-   * Удаляет файл с заданным ID
-   * @param id - id файла, который нужно удалить
-   * @return - объект UserResponseDto
-   */
   UserResponseDto deleteById(UUID id);
-  /**
-   * Возвращает юзера по его ID
-   * @param id - ID юзера
-   * @return - объект юзера с заданным ID
-   */
-  UserResponseDto findById(UUID id);
 
   List<FileMetadataDto> findAllFilesByLogin(String login);
 
--- user-service/src/main/java/ru/answer_42/user_service/service/impl/UserServiceImpl.java
+++ user-service/src/main/java/ru/answer_42/user_service/service/impl/UserServiceImpl.java
@@ -1,6 +1,5 @@
 package ru.answer_42.user_service.service.impl;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
@@ -14,14 +13,14 @@ import ru.answer_42.user_service.mapper.FileMetadataMapper;
 import ru.answer_42.user_service.mapper.UserMapper;
 import ru.answer_42.user_service.model.FileMetadata;
 import ru.answer_42.user_service.model.User;
-import ru.answer_42.user_service.repository.InMemoryUserRepository;
+import ru.answer_42.user_service.repository.UserRepository;
 import ru.answer_42.user_service.service.UserService;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
-  private final InMemoryUserRepository userRepository;
+  private final UserRepository userRepository;
   private final FileMetadataMapper fileMetadataMapper;
   private final UserMapper userMapper;
 
@@ -35,53 +34,54 @@ public class UserServiceImpl implements UserService {
   @Override
   public UserResponseDto save(UserRequestDto userRequestDto) {
     User user = userMapper.toEntity(userRequestDto);
-    User savedUser = userRepository.save(UUID.randomUUID(), user);
+    User savedUser = userRepository.save(user);
     return userMapper.toUserResponseDto(savedUser);
   }
 
   @Override
   public UserResponseDto update(UUID id, UserRequestDto userRequestDto) {
     User existingUser = userRepository.findById(id)
-        .orElseThrow(() -> new ResourceNotFoundException("Course not found with id: " + id));
+        .orElseThrow(() -> new ResourceNotFoundException("User not found with id: " + id));
 
     userMapper.updateEntityFromDto(userRequestDto, existingUser);
 
-    User updateFile = userRepository.update(existingUser);
+    User updateFile = userRepository.save(existingUser);
 
     return userMapper.toUserResponseDto(updateFile);
   }
 
   @Override
   public UserResponseDto deleteById(UUID id) {
-    findById(id);
-    return userMapper.toUserResponseDto(userRepository.deleteById(id));
-  }
-
-  public FileMetadata addFileMetadata(FileMetadata fileMetadata){
-    String login = fileMetadata.getUserLogin();
-    User user = userRepository.findByLogin(login).orElse(userRepository.save(UUID.randomUUID(), new User(null,login,null, new ArrayList<>(),null,null)));
-    FileMetadata resultFileMetadata = userRepository.addFileMetadata(user, fileMetadata);
-    return resultFileMetadata;
-  }
-
-  @Override
-  public UserResponseDto findById(UUID id) {
-    User user = userRepository.findById(id).
-        orElseThrow(() -> new ResourceNotFoundException("User not found with id: " + id));
+    User user = userRepository.findById(id)
+        .orElseThrow(() -> new ResourceNotFoundException("User not found with id: " + id));
+    userRepository.deleteById(id);
     return userMapper.toUserResponseDto(user);
   }
+
+  public FileMetadata addFileMetadata(FileMetadata fileMetadata) {
+    String login = fileMetadata.getUserLogin();
+    User user = userRepository.findByLogin(fileMetadata.getUserLogin())
+        .orElseThrow(() -> new ResourceNotFoundException("User not found with login: " + login));
+    user.getFiles().add(fileMetadata);
+    userRepository.save(user);
+    return fileMetadata;
+  }
+
 
   @Override
   public List<FileMetadataDto> findAllFilesByLogin(String login) {
     User user = userRepository.findByLogin(login).
-        orElseThrow(() -> new ResourceNotFoundException("User not found with login: " + login));;
+        orElseThrow(() -> new ResourceNotFoundException("User not found with login: " + login));
+    ;
     return user.getFiles().stream().map(fileMetadataMapper::toFileMetadataDto).toList();
   }
 
   @Override
   public List<FileDownloadDto> findAllLinksByLogin(String login) {
     User user = userRepository.findByLogin(login).
-        orElseThrow(() -> new ResourceNotFoundException("User not found with login: " + login));;
-    return user.getFiles().stream().map(FileMetadata::getDownloadUrl).map(FileDownloadDto::new).toList();
+        orElseThrow(() -> new ResourceNotFoundException("User not found with login: " + login));
+    ;
+    return user.getFiles().stream().map(FileMetadata::getDownloadUrl).map(FileDownloadDto::new)
+        .toList();
   }
 }
